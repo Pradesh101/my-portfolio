700732b3e1c85f3c3566e4b313c7f518a37bd0f9
Pradesh101 <pradesh248@gmail.com>
2025-08-11 16:14:31 -0500

    update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -10,7 +10,7 @@ const AboutSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
             <h3 className="text-2xl font-semibold">
-              Passionate Full Stack Web Developer
+              Passionate Software Engineer
             </h3>
             <p className="text-muted-foreground">
               With over 5 years of experience in full stack web development, I
@@ -30,12 +30,24 @@ const AboutSection = () => {
                 Get In Touch
               </a>
 
-              <a
-                href=""
+              {/* <button
+                onClick={() => {
+                  window.open("../assets/CV_Pradesh_Gwachha.pdf", "_blank");
+                }}
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
-              </a>
+              </button> */}
+              <button
+                onClick={() => {
+                  const cvFile = "/CV_Pradesh_Gwachha.pdf";
+                  const link = window.open(cvFile, "_blank");
+                  link.document.title = "CV - Pradesh Gwachha";
+                }}
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+              >
+                Download CV
+              </button>
             </div>
           </div>
           <div className="grid grid-cols-1 gap-6">
